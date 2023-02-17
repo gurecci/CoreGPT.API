3c47cada44466486b6a24f9ca3aef427d0763697
Fatih Güreci <fatih.guereci@stecher-automation.de>
2023-02-17 22:04:45 +0300

    OpenAIController

---
+++ CoreGPT.API/Controllers/OpenAIController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenAI_API;
+using OpenAI_API.Completions;
+
+namespace OpenAIApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OpenAIController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetData(string input)
+        {
+            string apiKey = "YourOpen AI key";
+            string response = "";
+            OpenAIAPI openai = new OpenAIAPI(apiKey);
+            CompletionRequest completion = new CompletionRequest();
+            completion.Prompt = input;
+            completion.Model = "text-davinci-003";
+            completion.MaxTokens = 4000;
+            var output = await openai.Completions.CreateCompletionAsync(completion);
+            if (output != null)
+            {
+                foreach (var item in output.Completions)
+                {
+                    response = item.Text;
+                }
+                return Ok(response);
+            }
+            else
+            {
+                return BadRequest("Not found");
+            }
+        }
+    }
+}
